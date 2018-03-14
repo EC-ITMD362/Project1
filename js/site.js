46fc0258e490e7fef4e33cffd62e8d625e84037e
ecabrera796 <ecabrera796@gmail.com>
2018-03-13 23:25:01 -0500

    Added validation

--- js/site.js
+++ js/site.js
@@ -3,3 +3,11 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+function validation(){
+  var x = document.form["name"]["email"]["snap"].value;
+  if (x==""){
+    alert("Please fill in the blank spaces!");
+    return false;
+  }
+}
